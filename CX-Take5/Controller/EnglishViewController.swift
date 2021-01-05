d2bf2ffb83570f29100319a1a7d8e7ef6dadea88
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2021-01-05 01:00:47 +0100

    OOP refactoring part-1/2 done

---
+++ CX-Take5/Controller/EnglishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EnglishViewController.swift
+//  CX-Take5
+//
+//  Created by Paweł Kozioł on 05/01/2021.
+//  Copyright © 2021 Paweł Kozioł. All rights reserved.
+//
+
+import UIKit
+
+class EnglishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
